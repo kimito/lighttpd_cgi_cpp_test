3c5e59d2b061dc0239f12c8089602661f3e29116
Kimito Kasahara <1219kk@gmail.com>
2023-10-26 06:44:53 +0900

    star using cgicc

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include<iostream>
 
+#include "cgicc/Cgicc.h"
+
 int main(int argc, char* argv[])
 {
+    cgicc::Cgicc cgi;
+    auto env = cgi.getEnvironment();
+
     std::cout << "Content-type: text/plain" << "\r\n";
     std::cout << "\r\n";
     std::cout << "hello api!" << "\r\n";
+    std::cout << "path: " << env.getPathInfo() << "\r\n";
+    std::cout << "query: " << env.getQueryString() << "\r\n";
 
     return 0;
 }

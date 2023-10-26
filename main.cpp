49778260ffac1ea6ea8ddd84ba4c998e69a92f00
Kimito Kasahara <1219kk@gmail.com>
2023-10-26 09:01:01 +0900

    return an image using OpenCV

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 
-#include "cgicc/Cgicc.h"
-#include <cgicc/HTTPContentHeader.h>
+#include"cgicc/Cgicc.h"
+#include<cgicc/HTTPContentHeader.h>
 
-void print_api_info()
+#include"opencv2/opencv.hpp"
+
+void print_api_info(const cgicc::CgiEnvironment &env)
 {
     std::cout << cgicc::HTTPContentHeader("text/plain");
 
@@ -12,12 +14,25 @@ void print_api_info()
     std::cout << "query: " << env.getQueryString() << "\r\n";
 }
 
+void print_image()
+{
+    std::cout << cgicc::HTTPContentHeader("image/jpeg");
+
+    cv::Mat img = cv::imread("./berries.jpg");
+    std::vector<uchar>buf;
+    cv::imencode(".jpg", img, buf);
+
+    std::cout.write(reinterpret_cast<const char*>(buf.data()), buf.size());
+}
+
 int main(int argc, char* argv[])
 {
     cgicc::Cgicc cgi;
     auto env = cgi.getEnvironment();
 
-    print_api_info();   
+    // print_api_info(env);
+
+    print_image();
 
     return 0;
 }
